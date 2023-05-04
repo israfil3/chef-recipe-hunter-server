35eb66f9efc8610725b5d642b7ca9432d0f7d1d9
unknown <mdisrafil61@gmail.com>
2023-05-04 18:23:46 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = 3000
+
+
+app.use(cors())
+const allData = require('./data.json')
+
+app.get('/data',(req,res)=>{
+  res.send(allData)
+})
+app.get('/data/:id', (req, res) => {
+  const id = req.params.id;
+    const selectedData = allData.find(n => n.id == id);
+    res.send({selectedData})
+})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
